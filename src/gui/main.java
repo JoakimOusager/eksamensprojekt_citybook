b1aafaddf1a1c06f93e570dd0beec4398139ea6c
blackjarnold <Jarl@Jarls-MacBook-Pro.local>
2017-05-05 12:56:15 +0200

    arkitektur

---
+++ src/gui/main.java
@@ -0,0 +1,18 @@
+package gui;/**
+ * Created by Jarl on 05/05/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
