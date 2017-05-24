c6f28c5275adde8d033b142b20a2099959478cbd
Jarl Eriksen <jarl@jarleriksen.dk>
2017-05-24 14:52:29 +0200

    Tilføjet Amazon database

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -7,9 +7,9 @@ public class DAO {
 
     //JDBC Driver name and Database URL
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/cbcrm?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    static final String DB_URL = "jdbc:mysql://citybook.ccha1lswmjtz.us-west-2.rds.amazonaws.com:3306/cbcrm";
 
     //Database credentials
-    static final String USER = "root";
-    static final String PASS = "root";
+    static final String USER = "citybook";
+    static final String PASS = "citybook";
 }
