fbb2985a1b189ae3c60276b66f80351540dc78cf
joakimousager <joak1584@stud.kea.dk>
2017-05-30 23:27:42 +0200

    Nyeste

--- src/dataaccess/DAO.java
+++ src/dataaccess/DAO.java
@@ -9,6 +9,7 @@ public class DAO {
     //JDBC Driver name and Database URL
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://citybook.ccha1lswmjtz.us-west-2.rds.amazonaws.com:3306/cbcrm";
+   // static final String DB_URL = "jdbc:mysql://localhost:3306/cbcrm";
 
     //Database credentials
     static final String USER = "citybook";
